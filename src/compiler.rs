a612b97be6bba9bdaee6a0250bbd72455fa6d120
certainty <david.krentzlin@gmail.com>
2021-03-31 16:17:07 +0200

    WIP

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,27 +1,3 @@
-pub mod backend;
 pub mod frontend;
 pub mod source;
-
-use super::vm::byte_code::chunk;
-use frontend::parser;
-use thiserror::Error;
-
-#[derive(Error, Debug)]
-pub enum CompileError {
-    #[error(transparent)]
-    ParseError(#[from] parser::ParseError),
-    #[error(transparent)]
-    CodegenerationError(#[from] backend::byte_code::GenerationError),
-}
-
-type Result<T> = std::result::Result<T, CompileError>;
-
-pub fn jit_compile<T: source::Source>(source: &mut T) -> Result<Option<chunk::Chunk>> {
-    if let Some(ast) = parser::parse(source)? {
-        let mut chunk = backend::byte_code::generate(&ast)?;
-        backend::byte_code::finalise(&mut chunk);
-        Ok(Some(chunk))
-    } else {
-        Ok(None)
-    }
-}
+pub mod source_location;
