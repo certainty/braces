7e7d5c2abe6be38111e83505c3ce022445e74522
certainty <david.krentzlin@gmail.com>
2021-04-01 11:34:11 +0200

    Add benchmark

---
+++ benches/compiler_benchmark.rs
@@ -0,0 +1,15 @@
+use braces::compiler::source::*;
+use braces::compiler::Compiler;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn compiler_benchmark(c: &mut Criterion) {
+    let mut compiler = Compiler::new();
+    let mut source = StringSource::new("#true", "bench");
+
+    c.bench_function("compile", |b| {
+        b.iter(|| compiler.compile(black_box(&mut source)))
+    });
+}
+
+criterion_group!(benches, compiler_benchmark);
+criterion_main!(benches);
