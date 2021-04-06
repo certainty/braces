1144fefead4cd570fe69cfd535706f99b6ece43f
certainty <david.krentzlin@gmail.com>
2021-04-06 15:12:42 +0200

    Boilerplate

--- src/compiler/frontend/parser/sexp.rs
+++ src/compiler/frontend/parser/sexp.rs
@@ -5,28 +5,57 @@ use nom::branch::alt;
 use nom::bytes::streaming::{is_not, tag, take_while_m_n};
 use nom::character::streaming::{char, multispace1};
 use nom::combinator::{map, map_opt, map_res, value, verify};
-use nom::error::{FromExternalError, ParseError};
+use nom::error::{ErrorKind, FromExternalError, ParseError};
 use nom::multi::fold_many0;
 use nom::sequence::{delimited, preceded};
 use nom::IResult;
 use nom_locate::LocatedSpan;
+use std::convert::From;
 
-struct Error {}
+#[derive(Debug)]
+pub enum Error {
+    IoError(std::io::Error),
+    FixMe,
+    Nom(ErrorKind),
+}
+
+impl<I> ParseError<I> for Error {
+    fn from_error_kind(_: I, kind: ErrorKind) -> Self {
+        Error::Nom(kind)
+    }
+
+    fn append(_: I, _: ErrorKind, other: Self) -> Self {
+        other
+    }
+}
+
+impl<I> FromExternalError<I, std::num::ParseIntError> for Error {
+    fn from_external_error(_: I, _: nom::error::ErrorKind, _: std::num::ParseIntError) -> Self {
+        todo!()
+    }
+}
+
+impl<I> FromExternalError<I, std::io::Error> for Error {
+    fn from_external_error(_: I, _: nom::error::ErrorKind, e: std::io::Error) -> Self {
+        Error::IoError(e)
+    }
+}
 
 type Result<'a, T> = IResult<Span<'a>, T, Error>;
 
-type Span<'a> = LocatedSpan<&'a str>;
+type Span<'a> = LocatedSpan<&'a str, SourceType>;
 
+#[derive(Debug, PartialEq)]
 struct Datum<'a> {
-    pub position: Span<'a>,
+    pub location: Option<Span<'a>>,
     value: Value,
 }
 
 /// R7RS
 impl<'a> Datum<'a> {
-    fn with_span(value: Value, span: &Span<'a>) -> Self {
+    fn with_location(value: Value, location: &Span<'a>) -> Self {
         Datum {
-            position: *span,
+            location: Some(location.to_owned()),
             value,
         }
     }
@@ -36,19 +65,70 @@ impl<'a> Datum<'a> {
 struct Parser {}
 
 impl Parser {
-    fn parse_char<'a>() -> Result<'a, Value> {
+    pub fn parse<'a, T: Source>(&self, source: &'a mut T) -> std::result::Result<Datum, Error> {
+        let source_type = source.source_type();
+
+        match source.as_str() {
+            Err(e) => Err(Error::IoError(e)),
+            Ok(buffer) => {
+                let input = Span::new_extra(buffer, source_type);
+                match self.parse_datum(input) {
+                    Ok(v) => Ok(v.1),
+                    Err(e) => Err(Error::FixMe),
+                }
+            }
+        }
+    }
+
+    fn parse_datum<'a>(&self, input: Span<'a>) -> Result<'a, Datum> {
         todo!()
     }
 
-    fn parse_char_literal<'a>() -> Result<'a, char> {
+    fn parse_char<'a>(&self, input: Span<'a>) -> Result<'a, Datum> {
         todo!()
     }
 
-    fn parse_named_char_literal<'a>(input: Span<'a>) -> Result<'a, char> {
+    fn parse_char_literal<'a>(&self, input: Span<'a>) -> Result<'a, char> {
+        todo!()
+    }
+
+    fn parse_named_char_literal<'a>(&self, input: Span<'a>) -> Result<'a, char> {
         value('\n', tag("newline"))(input)
     }
 
-    fn parse_hex_char_literal<'a>() -> Result<'a, char> {
+    fn parse_hex_char_literal<'a>(&self, input: Span<'a>) -> Result<'a, char> {
         todo!()
+    }
+}
+
+impl Default for Parser {
+    fn default() -> Self {
+        Self {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::compiler::source::{Source, StringSource};
+
+    #[test]
+    fn test_read_character() {
+        let parser = Parser::default();
+        let mut source = src("");
+        let source_type = source.source_type();
+
+        source = src("#\\c");
+        let mut datum = parser.parse(&mut source).unwrap();
+
+        assert_eq!(datum.value, Value::character('c'));
+
+        source = src("#\\☆");
+        datum = parser.parse(&mut source).unwrap();
+        assert_eq!(datum.value, Value::character('☆'));
+    }
+
+    fn src(inp: &str) -> impl Source {
+        StringSource::new(inp, "datum-parser-test")
     }
 }
