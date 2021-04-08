ea159787e60083759d98825c957a1a025bcd3ef8
certainty <david.krentzlin@gmail.com>
2021-04-08 12:06:03 +0200

    Parse everything that pest parses

--- src/compiler/frontend/parser/sexp.rs
+++ src/compiler/frontend/parser/sexp.rs
@@ -3,13 +3,13 @@ use crate::compiler::source_location::SourceLocation;
 use crate::vm::scheme::value::Value;
 use nom::branch::alt;
 use nom::bytes::complete::{is_not, tag, take_while_m_n};
-use nom::character::complete::{anychar, char, multispace1, none_of};
+use nom::character::complete::{alpha1, anychar, char, line_ending, multispace1, none_of, one_of};
 use nom::combinator::{map, map_opt, map_res, value, verify};
 use nom::error::{
     context, ContextError, ErrorKind, FromExternalError, ParseError, VerboseError, VerboseErrorKind,
 };
 use nom::multi::{fold_many0, many0};
-use nom::sequence::{delimited, preceded, terminated};
+use nom::sequence::{delimited, pair, preceded, terminated, tuple};
 use nom::Err;
 use nom::IResult;
 use nom_locate::{position, LocatedSpan};
@@ -88,6 +88,7 @@ fn parse_datum<'a>(input: Input<'a>) -> ParseResult<'a, Datum> {
         alt((
             context("character", parse_character),
             context("boolean", parse_boolean),
+            context("symbol", parse_symbol),
             context("string", parse_string),
         )),
     )(input)
@@ -267,8 +268,156 @@ fn parse_string_literal<'a>(input: Input<'a>) -> ParseResult<'a, &'a str> {
     }
 }
 
+//////////////////////////////////////////
+// Identifier / Symbol
+/////////////////////////////////////////
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum SymbolElement<'a> {
+    Literal(&'a str),
+    EscapedChar(char),
+}
+
+fn parse_symbol<'a>(input: Input<'a>) -> ParseResult<'a, Datum> {
+    let symbol_literal = alt((
+        parse_identifier,
+        parse_delimited_identifier,
+        parse_peculiar_identifier,
+    ));
+
+    map_datum(symbol_literal, Value::symbol)(input)
+}
+
+#[inline]
+fn parse_peculiar_identifier<'a>(input: Input<'a>) -> ParseResult<'a, String> {
+    let explicit_sign_str = map(parse_explicit_sign, String::from);
+
+    alt((
+        parse_peculiar_with_sign,
+        parse_peculiar_with_sign_dot,
+        parse_peculiar_with_dot,
+        explicit_sign_str,
+    ))(input)
+}
+
+#[inline]
+fn parse_peculiar_with_sign<'a>(input: Input<'a>) -> ParseResult<'a, String> {
+    let (s, (sign, sign_sub, subseq)) = tuple((
+        parse_explicit_sign,
+        parse_sign_subsequent,
+        many0(parse_sign_subsequent),
+    ))(input)?;
+
+    let mut symbol = String::from(sign);
+    symbol.push(sign_sub);
+    symbol.extend(subseq.iter());
+
+    Ok((s, symbol))
+}
+
+#[inline]
+fn parse_peculiar_with_dot<'a>(input: Input<'a>) -> ParseResult<'a, String> {
+    let (s, (dot, dot_subseq, subseq)) =
+        tuple((char('.'), parse_dot_subsequent, many0(parse_subsequent)))(input)?;
+
+    let mut symbol = String::from(dot);
+    symbol.push(dot_subseq);
+    symbol.extend(subseq.iter());
+
+    Ok((s, symbol))
+}
+
+#[inline]
+fn parse_peculiar_with_sign_dot<'a>(input: Input<'a>) -> ParseResult<'a, String> {
+    let (s, (sign, sign_sub, dot_subseq)) =
+        tuple((parse_explicit_sign, char('.'), parse_dot_subsequent))(input)?;
+
+    let mut symbol = String::from(sign);
+    symbol.push(sign_sub);
+    symbol.push(dot_subseq);
+
+    Ok((s, symbol))
+}
+
+#[inline]
+fn parse_dot_subsequent<'a>(input: Input<'a>) -> ParseResult<'a, char> {
+    alt((parse_sign_subsequent, char('.')))(input)
+}
+
+#[inline]
+fn parse_sign_subsequent<'a>(input: Input<'a>) -> ParseResult<'a, char> {
+    alt((parse_initial, parse_explicit_sign, char('@')))(input)
+}
+
+#[inline]
+fn parse_delimited_identifier<'a>(input: Input<'a>) -> ParseResult<'a, String> {
+    let symbol_elements = fold_many0(
+        parse_symbol_element,
+        String::new(),
+        |mut string, element| {
+            match element {
+                SymbolElement::Literal(s) => string.push_str(s),
+                SymbolElement::EscapedChar(c) => string.push(c),
+            }
+            string
+        },
+    );
+
+    delimited(char('|'), symbol_elements, char('|'))(input)
+}
+
+#[inline]
+fn parse_symbol_element<'a>(input: Input<'a>) -> ParseResult<'a, SymbolElement<'a>> {
+    let parse_symbol_escape = value('|', tag("\\|"));
+
+    alt((
+        map(parse_mnemonic_escape, SymbolElement::EscapedChar),
+        map(parse_inline_hex_escape, SymbolElement::EscapedChar),
+        map(parse_symbol_escape, SymbolElement::EscapedChar),
+        map(parse_symbol_literal, SymbolElement::Literal),
+    ))(input)
+}
+
+#[inline]
+fn parse_symbol_literal<'a>(input: Input<'a>) -> ParseResult<'a, &'a str> {
+    let (s, v) = is_not("|\\")(input)?;
+
+    Ok((s, v.fragment()))
+}
+
+fn parse_identifier<'a>(input: Input<'a>) -> ParseResult<'a, String> {
+    let mut identifier = String::new();
+    let (s, (init, subseq)) = pair(parse_initial, many0(parse_subsequent))(input)?;
+
+    identifier.push(init);
+    identifier.extend(subseq.iter());
+
+    Ok((s, identifier))
+}
+
+#[inline]
+fn parse_initial<'a>(input: Input<'a>) -> ParseResult<'a, char> {
+    let letter = verify(anychar, |c| c.is_alphabetic());
+    let special_initial = one_of("!$%&*/:<=>?^_~");
+
+    alt((letter, special_initial))(input)
+}
+
+#[inline]
+fn parse_subsequent<'a>(input: Input<'a>) -> ParseResult<'a, char> {
+    let digit = verify(anychar, |c| c.is_digit(10));
+    let special_subsequent = alt((parse_explicit_sign, char('.'), char('@')));
+
+    alt((parse_initial, digit, special_subsequent))(input)
+}
+
+#[inline]
+fn parse_explicit_sign<'a>(input: Input<'a>) -> ParseResult<'a, char> {
+    alt((char('+'), char('-')))(input)
+}
+
 fn consume_line_ending<'a>(input: Input<'a>) -> ParseResult<'a, ()> {
-    value((), alt((tag("\r\n"), tag("\n"), tag("\r"))))(input)
+    value((), line_ending)(input)
 }
 
 fn parse_intra_line_ws<'a>(input: Input<'a>) -> ParseResult<'a, ()> {
@@ -334,7 +483,43 @@ mod tests {
     #[test]
     fn test_read_string_bugs() {
         assert_parse_as("\"\"", Value::string(""));
-        //    assert_parse_ok("\"–)ꍽ[\u{83}\u{2}\u{94}\u{10}\u{1e}(\u{9f}\u{94}\t^+\u{fff5}\u{2003}JX}]\u{9f}VL%®\u{81}{e@8\u{2}\u{9c}{\u{83}\u{1b}\\7/O^7x\u{19}v¤ᣋ\u{9a}^~§\u{83}02x!)\u{3b19f}f}\u{8d}>5\u{8c}{}\u{52bf9}\u{1f}徒1\u{c73ef}骍<\u{1a}v^t\u{95}\u{92}6l쏊b\u{10fffe}\\015\u{0}¯8\u{8}\"");
+        assert_parse_as(r#""\\7""#, Value::string("\\7"));
+    }
+
+    #[test]
+    fn test_read_symbol() {
+        let symbols = vec![
+            "<=?",
+            "->string",
+            "a34kTMNs",
+            "lambda",
+            "list->vector",
+            "q",
+            "V17a",
+            "the-word-recursion-has-many-meanings",
+        ];
+
+        for sym in symbols.iter() {
+            assert_parse_as(sym, Value::symbol(*sym))
+        }
+    }
+
+    #[test]
+    fn test_read_symbol_delimited() {
+        assert_parse_as("||", Value::symbol(""));
+
+        assert_parse_as("|two words|", Value::symbol("two words"));
+        assert_parse_as(r#"|two\x20;words|"#, Value::symbol("two words"));
+        assert_parse_as(r#"|two\|words|"#, Value::symbol("two|words"));
+
+        assert_parse_as(
+            r#"|test with \| escaped vertical lines|"#,
+            Value::symbol("test with | escaped vertical lines"),
+        );
+        assert_parse_as(
+            r#"|:(\x80;\xfff6;]&\x5c;"|"#,
+            Value::symbol(":(\u{0080}\u{fff6}]&\\\""),
+        );
     }
 
     fn assert_parse_as(inp: &str, expected: Value) {
