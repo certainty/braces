445c875efbe885af348a203d22d9ddddd523c84b
certainty <david.krentzlin@gmail.com>
2021-06-04 17:54:57 +0200

    Add fib-iter to example

--- examples/fib.scm
+++ examples/fib.scm
@@ -1,3 +1,12 @@
+(define fib-tc (lambda (n)
+  (fib-iter 1 0 n)))
+
+(define fib-iter (lambda (a b count)
+  (if (= count 0)
+      b
+      (fib-iter (+ a b) a (- count 1)))))
+
+
 (define fib (lambda (n) (if (<= n 2) 1 (+ (fib (- n 1)) (fib (- n 2))))))
 
 (fib 10)
