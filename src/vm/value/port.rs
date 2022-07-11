5078ca9b51bf14e9587b1549c54d6c024a8f1e97
certainty <david.krentzlin@gmail.com>
2022-07-11 12:54:46 +0200

    WIP

--- src/vm/value/port.rs
+++ src/vm/value/port.rs
@@ -1,7 +1,43 @@
 use std::io::{BufWriter, Write};
 
+pub type Result<T> = std::result::Result<T, super::error::RuntimeError>;
+
 #[derive(Debug)]
-pub enum Port {
-    InputPort(),
-    OutputPort(BufWriter<Write>),
+pub enum Content<T> {
+    Eof,
+    Data(T),
+}
+
+pub trait SchemePort {
+    fn is_binary_port() -> bool;
+    fn is_textual_port() -> bool;
+    fn is_input_port() -> bool;
+}
+
+pub trait BinaryOutputPort {
+    fn write_u8(&mut self, byte: u8) -> Result<usize>;
+    fn flush(&mut self) -> Result<()>;
+    fn close(&self) -> Result<()>;
+}
+
+pub trait BinaryInputPort {
+    fn read_u8(&mut self) -> Result<Content<u8>>;
+    fn peek(&self) -> Result<Content<u8>>;
+    fn is_ready(&self) -> bool;
+    fn close(&self) -> Result<()>;
+}
+
+pub trait TextualOutputPort {
+    fn write_char(&mut self, c: char) -> Result<usize>;
+    fn write(&mut self, buf: &str) -> Result<usize>;
+    fn flush(&mut self) -> Result<()>;
+    fn close(&self) -> Result<()>;
+}
+
+pub trait TextualInputPort {
+    fn read_line(&mut self) -> Result<Content<String>>;
+    fn read_char(&mut self) -> Result<Content<char>>;
+    fn peek_char(&mut self) -> Result<Content<char>>;
+    fn is_ready(&self) -> bool;
+    fn close(&self) -> Result<()>;
 }
